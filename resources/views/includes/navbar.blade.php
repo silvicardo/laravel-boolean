703a5330ef019799735f57ac095b280ec4ac5858
Riccardo Silvi <silvicardo@gmail.com>
2019-02-19 07:48:02 +0100

    navbar layout

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,3 +1,29 @@
-<nav>
-  i am the navbar
+<nav class="navbar navbar-expand-lg navbar-light px-5">
+  <a class="navbar-brand" href="#">
+    <img src="https://www.boolean.careers/images/common/logo.png" alt="">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarsExample05">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">corso</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">metodo</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">dopo il corso</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">faq</a>
+      </li>
+    </ul>
+    <a class="btn_orange text-capitalize" href="">iscriviti</a>
+  </div>
 </nav>
