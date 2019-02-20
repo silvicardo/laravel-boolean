41d5be0ff764722831c03d1ed770181581537770
Riccardo Silvi <silvicardo@gmail.com>
2019-02-20 15:27:37 +0100

    students config file

---
+++ config/studenti.php
@@ -0,0 +1,52 @@
+
+<?php
+return [
+    [
+        'name' => 'Alessandro Biagini',
+        'slug' => 'alessandro-biagini',
+        'age' => 24,
+        'azienda' => 'Softec spa',
+        'img' => 'https://www.boolean.careers/images/students/biagini.png',
+        'gender' => 'm'
+    ],
+    [
+        'name' => 'Davide Lecci',
+        'slug' => 'davide-lecci',
+        'age' => 28,
+        'azienda' => 'Facile.it',
+        'img' => 'https://www.boolean.careers/images/students/lecci.png',
+        'gender' => 'm'
+    ],
+    [
+        'name' => 'Loana Masetti',
+        'slug' => 'loana-masetti',
+        'age' => 35,
+        'azienda' => 'Cayenne',
+        'img' => 'https://www.boolean.careers/images/students/masetti.png',
+        'gender' => 'f'
+    ],
+    [
+        'name' => 'Samuel Poma',
+        'slug' => 'samuel-poma',
+        'age' => 26,
+        'azienda' => 'Libero professionista',
+        'img' => 'https://www.boolean.careers/images/students/poma.png',
+        'gender' => 'm'
+    ],
+    [
+        'name' => 'Federico Losi',
+        'slug' => 'federico-losi',
+        'age' => 30,
+        'azienda' => 'Facile.it',
+        'img' => 'https://www.boolean.careers/images/students/losi.png',
+        'gender' => 'm'
+    ],
+    [
+        'name' => 'Ervin Bani',
+        'slug' => 'ervin-bani',
+        'age' => 31,
+        'azienda' => 'Fabbrica digitale',
+        'img' => 'https://www.boolean.careers/images/students/bani.png',
+        'gender' => 'm'
+    ]
+];
